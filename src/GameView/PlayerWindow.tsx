936238dbf6559f1984acd66f62fbb2927c49077d
Kaylem Brown-Malone <kaylem20201@gmail.com>
2023-07-28 09:18:38 -0700

    Okay THIS should fix the image problem (I can't test this locally wahhh)

--- src/GameView/PlayerWindow.tsx
+++ src/GameView/PlayerWindow.tsx
@@ -7,7 +7,7 @@ function PlayerWindow (props : {player: Player, victor: boolean}) {
 
   function MoveImage() {
     const imgName = props.player.move.imgName != null ? props.player.move.imgName : move
-    const imageLoc = '/frame-game/' + name + '/' + imgName + '.png';
+    const imageLoc = name + '/' + imgName + '.png';
     const image = <img src={imageLoc} alt={move} />;
 
     return (
