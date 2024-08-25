3b9bf477ec54087adab8766eefe992a9732a1635
Kaylem <kaylem20201@gmail.com>
2024-08-24 22:06:06 -0700

    Fix css for rest of main layout

--- app/GameView/GameView.tsx
+++ app/GameView/GameView.tsx
@@ -75,8 +75,8 @@ function GameView() {
   }
 
   return (
-    <section className="h-full">
-      <div className="titles text-3xl text-red-500 text-center w-full m-auto font-bold font-sans uppercase">
+    <section className="h-full flex flex-col justify-stretch">
+      <div className="titles text-3xl text-red-500 text-center w-full font-bold font-sans uppercase">
         <h1>Matchup</h1>
         <div className="versus-flex flex flex-row flex-nowrap justify-center">
           <h2 className="charName basis-5/12">{matchup ? (matchup.player1.charName).replace(/_/g, ' ') : ''}</h2>
@@ -84,15 +84,15 @@ function GameView() {
           <h2 className="charName basis-5/12">{matchup ? (matchup.player2.charName).replace(/_/g, ' ') : ''}</h2>
         </div>
       </div>
-      <div className="characterContainer">
-        <div className="playerOne characterWindow">
+      <div className="characterContainer flex-grow flex flex-row flex-nowrap justify-center">
+        <div className="playerOne characterWindow basis-1/2 flex flex-col justify-center">
           {matchup ? <PlayerWindow player={matchup.player1} victor={(victor === PlayerOption.player1) ? true : false} /> : null}
         </div>
-        <div className="playerTwo characterWindow">
+        <div className="playerTwo characterWindow basis-1/2 flex flex-col justify-center -scale-x-100">
           {matchup ? <PlayerWindow player={matchup.player2} victor={victor === PlayerOption.player2} /> : null}
         </div>
       </div>
-      <div className="interactionContainer center">
+      <div className="interactionContainer text-center h-fit bg-rose-700">
         <GameHelp />
         {matchState === MatchStates.end ? (<GameEndContainer isWinner={userGuess === victor} resetGame={resetGame} />) : null}
         <MoveNameContainer input1={matchup ? matchup.player1.moveData.input : 'Loading'} input2={matchup ? matchup.player2.moveData.input : 'Loading'} onUserGuess={onUserGuess} />
