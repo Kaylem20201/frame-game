8c4b71f222db2e78d6cf95001efd721e80c22203
Kaylem <kaylem20201@gmail.com>
2024-08-25 11:47:36 -0700

    Fix import

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -1,9 +1,9 @@
 'use server';
 import { Matchup, Move, Player } from './interfaces';
 import { getCharacterList, getCharacterMoves } from './fetching';
-import { gameAbbreviations } from './enums';
+import { GameAbbreviations } from './enums';
 
-export async function genNewMatchup(game: gameAbbreviations): Promise<Matchup> {
+export async function genNewMatchup(game: GameAbbreviations): Promise<Matchup> {
   const player1prom = genRandomPlayer(game);
   const player2prom = genRandomPlayer(game);
   const [player1, player2] = await Promise.all([player1prom, player2prom]);
@@ -16,7 +16,7 @@ export async function genNewMatchup(game: gameAbbreviations): Promise<Matchup> {
   return matchup;
 };
 
-async function genRandomPlayer(game: gameAbbreviations): Promise<Player> {
+async function genRandomPlayer(game: GameAbbreviations): Promise<Player> {
   const charName = await getRandomCharName(game);
   const move = await getRandomMove(game, charName);
 
@@ -26,13 +26,13 @@ async function genRandomPlayer(game: gameAbbreviations): Promise<Player> {
   }
 }
 
-async function getRandomCharName(game: gameAbbreviations): Promise<string> {
+async function getRandomCharName(game: GameAbbreviations): Promise<string> {
   const charNames = await getCharacterList(game);
   const charName = charNames[Math.floor(Math.random() * charNames.length)];
   return charName;
 }
 
-async function getRandomMove(game: gameAbbreviations, charName: string): Promise<Move> {
+async function getRandomMove(game: GameAbbreviations, charName: string): Promise<Move> {
   const charaMoves = await getCharacterMoves(game, charName);
   if (!charaMoves) throw new Error();
   const resultMove = charaMoves[Math.floor(Math.random() * charaMoves.length)];
