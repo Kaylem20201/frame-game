f8e71bbc26e177c76925b8a66906a5995a53b7ac
Kaylem <kaylem20201@gmail.com>
2025-06-12 14:54:01 -0700

    Migrate React to v19, swagger-ui-react to swagger-ui-dist
    There was a dependency issue when trying to use HeroUI, as it requires
    React v19 which is incompatible with swagger-ui-react. swagger-ui-react
    doesn't look like it will be updated any time soon, so we've migrated to
    swagger-ui-dist for now (using methods posted at
    https://github.com/swagger-api/swagger-ui/issues/10243 as a template)

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -1,19 +1,12 @@
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import { parse, stringify } from "yaml";
 import fs from "fs";
+import PageContent from "./page-content";
+import { SwaggerUIBundle } from "swagger-ui-dist";
 
 //TODO: Switch to a public url for the schema after testing
 let schema = fs.readFileSync(process.cwd() + "/app/api/schema.yaml", "utf8");
 
-function APIWindow() {
-  //NOTE: As of 5/30/25, next in strict mode will put up an error due to Swagger using a deprecated unsafe component 
-  //May be addressed in PR #10373
-  return (
-    <>
-      <SwaggerUI spec={schema} />
-    </>
-  );
+async function APIWindow() {
+  return <PageContent schema={schema} />;
 }
 
 export default APIWindow;
